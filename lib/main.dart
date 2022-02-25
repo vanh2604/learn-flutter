c25a80faa90f7813a50749db07cdfeffeea3518c
vudinh2678@gmail.com <vudinh2678@gmail.com>
2022-02-25 14:08:07 +0700

    layout widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 void main() => runApp(const MyApp());
 
@@ -18,24 +17,109 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class ProductBox extends StatelessWidget {
+  const ProductBox({
+    Key? key,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  }) : super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      height: 120,
+      child: Card(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset("assets/appimages/" + image, width: 100, height: 100),
+          Expanded(
+              child: Container(
+            padding: const EdgeInsets.all(2),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 15),
+                ),
+                Text(
+                  "price ${price.toString()}",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ))
+        ],
+      )),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(widget.title),
         ),
-        body: ListView.builder(
-          itemBuilder: (_, index) {
-            return Container(
-                width: 500,
-                height: 500,
-                color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-                    .withOpacity(1.0));
-          },
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: const <Widget>[
+            ProductBox(
+                name: "iPhone",
+                description: "iPhone is the stylist phone ever",
+                price: 1000,
+                image: "iphone.jpg"),
+            ProductBox(
+                name: "Pixel",
+                description: "Pixel is the most featureful phone ever",
+                price: 800,
+                image: "pixel.jpeg"),
+            ProductBox(
+                name: "Laptop",
+                description: "Laptop is most productive development tool",
+                price: 2000,
+                image: "laptop.jpeg"),
+            ProductBox(
+                name: "Tablet",
+                description:
+                    "Tablet is the most useful device ever for meeting",
+                price: 1500,
+                image: "tablet.jpg"),
+            ProductBox(
+                name: "Pendrive",
+                description: "Pendrive is useful storage medium",
+                price: 100,
+                image: "pendrive.jpg"),
+            ProductBox(
+                name: "Floppy Drive",
+                description: "Floppy drive is useful rescue storage medium",
+                price: 20,
+                image: "floppydisk.png"),
+          ],
         ));
   }
 }
